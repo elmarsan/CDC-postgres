fefb993220309e9ee9517f5c2a194e2014065383
cerberusVT <mselias97@gmail.com>
2021-10-16 11:42:10 +0200

    refactor: use backticks on create function query, drop trigger if exits and rename getConnInfo

--- listen.go
+++ listen.go
@@ -37,7 +37,7 @@ func GetListener(event ListenEvent) (*pq.Listener, error) {
 }
 
 func connect(connParams DBConnParams) *sql.DB {
-	connInfo := getConnInfo(connParams)
+	connInfo := connInfo(connParams)
 
 	db, err := sql.Open("postgres", connInfo)
 	if err != nil {
@@ -48,36 +48,44 @@ func connect(connParams DBConnParams) *sql.DB {
 }
 
 func setupListener(db *sql.DB, event ListenEvent) (*pq.Listener, error) {
-	_, err := db.Query("" +
-		"CREATE OR REPLACE FUNCTION cdc_notify_event() RETURNS TRIGGER AS $$ " +
-		"DECLARE " +
-		"	data json;" +
-		"   notification json;" +
-		"" +
-		"BEGIN IF (TG_OP = 'DELETE') THEN " +
-		"	data = row_to_json(OLD);" +
-		"ELSE " +
-		"	data = row_to_json(NEW);" +
-		"END IF;" +
-		"" +
-		"notification = json_build_object(" +
-		"	'table',TG_TABLE_NAME, " +
-		"	'action', TG_OP," +
-		"	'data', data);" +
+	_, err := db.Query(`
+		CREATE OR REPLACE FUNCTION cdc_notify_event() RETURNS TRIGGER AS $$
 
-		"PERFORM pg_notify('events',notification::text);" +
-		"RETURN NULL;" +
-		"END;" +
-		"" +
-		"$$ LANGUAGE plpgsql;")
+		DECLARE 
+			data json;
+		    notification json;
+		
+		BEGIN 
+			IF (TG_OP = 'DELETE') THEN
+		 		data = row_to_json(OLD);
+			ELSE
+		 		data = row_to_json(NEW);
+			END IF;
+		 
+			notification = json_build_object(
+				'table',TG_TABLE_NAME, 
+				'action', TG_OP,
+				'data', data);
+
+			PERFORM pg_notify('events',notification::text);
+			RETURN NULL;
+		END;
+
+		$$ LANGUAGE plpgsql;
+	`)
+
 	if err != nil {
 		fmt.Println(err)
 		return nil, err
 	}
 
-	triggerRawQuery := fmt.Sprintf("CREATE TRIGGER %s_notify_event AFTER %s ON %s FOR EACH ROW EXECUTE PROCEDURE cdc_notify_event();", event.Table, event.Event, event.Table)
-
-	_, err = db.Query(triggerRawQuery)
+	_, err = db.Query(
+		fmt.Sprintf(`
+			DROP TRIGGER IF EXISTS %s_notify_event ON %s;
+			CREATE TRIGGER %s_notify_event AFTER %s ON %s FOR EACH ROW EXECUTE PROCEDURE cdc_notify_event();
+	`, event.Table, event.Table, event.Table, event.Event, event.Table,
+		),
+	)
 	if err != nil {
 		return nil, errors.New(fmt.Sprintf("Error: table `%s` does not exist", event.Table))
 	}
@@ -87,13 +95,13 @@ func setupListener(db *sql.DB, event ListenEvent) (*pq.Listener, error) {
 		return nil, err
 	}
 
-	return pq.NewListener(getConnInfo(event.ConnParams), time.Second, time.Second, func(ev pq.ListenerEventType, err error) {
+	return pq.NewListener(connInfo(event.ConnParams), time.Second, time.Second, func(ev pq.ListenerEventType, err error) {
 		if err != nil {
 			fmt.Println(err.Error())
 		}
 	}), nil
 }
 
-func getConnInfo(connParams DBConnParams) string {
+func connInfo(connParams DBConnParams) string {
 	return fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", connParams.Host, connParams.Port, connParams.User, connParams.Pass, connParams.Name)
 }
